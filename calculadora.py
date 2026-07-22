f78d2933a8d53df0ba91e008b6fc6191ed83a489
p8618653-byte <p8618653@gmail.com>
2026-07-22 15:39:02 -0500

    Update calculadora.py
    AGREGAR LA MULTIPLICACIÓN
    Dhanna Ignacio

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-## CALCULADORA - VERSIÓN 2
+## CALCULADORA - VERSIÓN 3
 # Autor(a): DHANNA IGNACIO
 # ===========================================
 print("CALCULADORA")
@@ -8,6 +8,7 @@ num2 = float(input("Ingrese el segundo número: "))
 
 print("\n 1. Sumar")
 print(" 2. Restar")
+print(" 3. Multiplicar")
 
 opcion = input("\n Seleccione una opción: ")
 
@@ -15,7 +16,10 @@ if opcion == "1":
     print("\n La suma es:", num1 + num2)
 
 elif opcion == "2":
-    print(\n La resta es:", num1 - num2)
+      print(\n La resta es:", num1 - num2)
+
+elif opcion == "3":
+      print("\n La multiplicación es:", num1 * num2)
 
 else:
     print("\n Opción no válida." )
