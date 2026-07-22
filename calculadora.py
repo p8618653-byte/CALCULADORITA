ec5ef754234436afa0910db7ed7c4af2afbbef25
p8618653-byte <p8618653@gmail.com>
2026-07-22 15:48:31 -0500

    Update calculadora.py
    AGREGAR DIVISIÓN
    By Dhanna Ignacio

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-## CALCULADORA - VERSIÓN 3
+## CALCULADORA - VERSIÓN 4
 # Autor(a): DHANNA IGNACIO
 # ===========================================
 print("CALCULADORA")
@@ -9,6 +9,7 @@ num2 = float(input("Ingrese el segundo número: "))
 print("\n 1. Sumar")
 print(" 2. Restar")
 print(" 3. Multiplicar")
+print(" 4. Dividir")
 
 opcion = input("\n Seleccione una opción: ")
 
@@ -21,5 +22,11 @@ elif opcion == "2":
 elif opcion == "3":
       print("\n La multiplicación es:", num1 * num2)
 
+elif opcion == "4":
+    if num2 != 0:
+        print("\n La División es:", num1 / num2)
+    else:
+        print("No se puede dividir entre cero.")
+        
 else:
     print("\n Opción no válida." )
